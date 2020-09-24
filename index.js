89650672b6c567f8b4ab74deccab25b43e45ccc6
Evan Peterson <evan.peterson216@gmail.com>
2020-09-24 12:59:30 -0400

    add set info to draft instance obj

--- index.js
+++ index.js
@@ -1,29 +1,60 @@
 const fs = require('fs');
 
 const LOG_NOTIFICATIONS = {
+  DRAFT_READY: '[UnityCrossThreadLogger]<== Event.ReadyDraft',
   DRAFT_INIT: '[UnityCrossThreadLogger]<== Draft.Notification',
   DRAFT_PACK_UPDATE: '[UnityCrossThreadLogger]Draft.Notify',
   DRAFT_PICK_WAS_MADE: '[UnityCrossThreadLogger]==> Draft.MakeHumanDraftPick'
 }
 
 const METHODS = {
+  DRAFT_READY: 'DraftReady', // Need to change this name or change log name
   DRAFT_START: 'DraftStart',
   CARDS_IN_PACK: 'CardsInPack',
   CARD_SELECTED: 'CardSelected'
 }
 
 const getEventType = (eventLog) => {
-  const { DRAFT_INIT, DRAFT_PACK_UPDATE, DRAFT_PICK_WAS_MADE } = LOG_NOTIFICATIONS;
-  const { DRAFT_START, CARDS_IN_PACK, CARD_SELECTED } = METHODS;
+  const { DRAFT_READY, DRAFT_INIT, DRAFT_PACK_UPDATE, DRAFT_PICK_WAS_MADE } = LOG_NOTIFICATIONS;
+  const { DRAFT_READY: READY, DRAFT_START, CARDS_IN_PACK, CARD_SELECTED } = METHODS;
+
+  const isDraftReadyEvent = (eventLog.indexOf(DRAFT_READY) !== -1) && (eventLog.indexOf('InternalEventName') !== -1);
   const isDraftInitEvent = (eventLog.indexOf(DRAFT_INIT) !== -1) && (eventLog.indexOf('SelfSeat') !== -1);
   const isDraftPackUpdateEvent = (eventLog.indexOf(DRAFT_PACK_UPDATE) !== -1);
   const isDraftPickMadeEvent = (eventLog.indexOf(DRAFT_PICK_WAS_MADE) !== -1);
 
+  if (isDraftReadyEvent) return READY;
   if (isDraftInitEvent) return DRAFT_START;
   if (isDraftPackUpdateEvent) return CARDS_IN_PACK;
   if (isDraftPickMadeEvent) return CARD_SELECTED;
   
   return null;
+}
+
+// Need to make this DRY later
+const extractDraftReadyData = (data) => {
+  try {
+    const [, parsedData] = data.split(LOG_NOTIFICATIONS.DRAFT_READY);
+    const { payload } = JSON.parse(parsedData);
+    return payload
+  } catch (e) {
+    console.error('Error in extractDraftReadyData', e);
+  }
+}
+
+const constructDraftReadyObj = (data) => {
+  const { InternalEventName, ModuleInstanceData } = extractDraftReadyData(data);
+  const [draftType, setName, setReleaseDate] = InternalEventName.split('_');
+  const { DraftId, ScreenName } = ModuleInstanceData['HumanDraft._internalState'];
+  // TODO maybe add slahes to date and use bo1 or bo3 for the draftType?
+  return {
+    method: METHODS.DRAFT_READY,
+    draftId: DraftId,
+    draftType,
+    setName,
+    setReleaseDate,
+    playerName: ScreenName,
+  }
 }
 
 const extractDraftInitData = (data) => {
@@ -47,16 +78,6 @@ const constructDraftInitObj = (data) => {
 }
 };
 
-const extractPlayerDraftPickData = (draftPickData) => {
-  try {
-    const [, parsedData] = draftPickData.split(LOG_NOTIFICATIONS.DRAFT_PICK_WAS_MADE);
-    const { request } = JSON.parse(parsedData);
-    return JSON.parse(request);
-  }catch (e) {
-    console.error('Error in extractPlayerDraftPickData', e);
-}
-}
-
 const constructDraftPackUpdateObj = (data) => {
   const [, cardsInPack] = data.split(LOG_NOTIFICATIONS.DRAFT_PACK_UPDATE);
   const parsedData = JSON.parse(cardsInPack);
@@ -68,6 +89,16 @@ const constructDraftPackUpdateObj = (data) => {
     cardsInPack: parsedData.PackCards.split(',').filter(Boolean),
   };
 };
+
+const extractPlayerDraftPickData = (draftPickData) => {
+  try {
+    const [, parsedData] = draftPickData.split(LOG_NOTIFICATIONS.DRAFT_PICK_WAS_MADE);
+    const { request } = JSON.parse(parsedData);
+    return JSON.parse(request);
+  }catch (e) {
+    console.error('Error in extractPlayerDraftPickData', e);
+  }
+}
 
 const constructDraftPickObj = (data) => {
   const { params: { draftId, cardId, packNumber, pickNumber } } = extractPlayerDraftPickData(data);
@@ -81,8 +112,12 @@ const constructDraftPickObj = (data) => {
 };
 
 const formatDraftLogs = (data) => {
-  const { DRAFT_START, CARDS_IN_PACK, CARD_SELECTED } = METHODS;
+  const { DRAFT_READY, DRAFT_START, CARDS_IN_PACK, CARD_SELECTED } = METHODS;
   const eventType = getEventType(data);
+
+  if (eventType === DRAFT_READY) {
+    return constructDraftReadyObj(data);
+  }
 
   if (eventType === DRAFT_START) {
     return constructDraftInitObj(data);
@@ -100,6 +135,16 @@ const formatDraftLogs = (data) => {
 }
 
 const condenseDraftData = (acc, draftData) => {
+
+  if (draftData.method === METHODS.DRAFT_READY) {
+    acc.push({
+      draftId: draftData.draftId,
+      draftType: draftData.draftType,
+      setName: draftData.setName,
+      setReleaseDate: draftData.setReleaseDate,
+      playerName: draftData.playerName,
+    });
+  }
 
   if (draftData.method === METHODS.CARDS_IN_PACK) {
     acc.push({
@@ -128,23 +173,23 @@ const groupByDraftId = (acc, draftData) => {
 
   if (!acc[draftData.draftId]) {
     acc[draftData.draftId] = {
-        set: null,
-        date: null,
-        draftOrder: []
+        // date: null, // TODO
+        ...draftData,
+        draftOrder: [],
     }
+    return acc
   }
 
   acc[draftData.draftId].draftOrder.push(draftData);
 
   return acc;
 }
-
+ 
 try {
-  const data = fs.readFileSync('./exampleTxt/examplePlayerLog2.txt');
+  const data = fs.readFileSync('./exampleTxt/twoDrafts.txt');
 
   // split contents by newline
-  const allDraftData = data
-      .toString()
+  const allDraftData = data.toString()
       .split(/\r?\n/)
       .filter(getEventType)
       .map(formatDraftLogs)
